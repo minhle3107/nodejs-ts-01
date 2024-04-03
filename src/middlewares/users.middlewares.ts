57a1db6bea9716bf7e8a13aaf6b6667ab037d159
minhle_3107 <lexuanminh3107@gmail.com>
2024-04-03 14:37:01 +0700

    express-validator

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
+import { checkSchema } from 'express-validator'
+import { validate } from '~/utils/validation'
 
 export const loginValidation = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body
@@ -8,10 +10,74 @@ export const loginValidation = (req: Request, res: Response, next: NextFunction)
   next()
 }
 
-export const registerValidation = (req: Request, res: Response, next: NextFunction) => {
-  const { email, password } = req.body
-  if (!email || !password) {
-    return res.status(400).json({ error: 'Missing email or password' })
-  }
-  next()
-}
+// export const registerValidation = (req: Request, res: Response, next: NextFunction) => {
+//   const { email, password } = req.body
+//   if (!email || !password) {
+//     return res.status(400).json({ error: 'Missing email or password' })
+//   }
+//   next()
+// }
+
+export const registerValidation = validate(
+  checkSchema({
+    name: {
+      notEmpty: true,
+      isLength: {
+        options: { min: 2, max: 255 },
+        errorMessage: 'Name must be between 2 and 255 characters'
+      },
+      trim: true
+    },
+    email: {
+      notEmpty: true,
+      isEmail: true,
+      trim: true
+    },
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: { min: 6, max: 50 },
+        errorMessage: 'Password must be at least 6 characters long and less than 50 characters'
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage: 'Password must contain at least 6 characters, 1 lowercase, 1 uppercase, 1 number, and 1 symbol'
+      }
+    },
+    confirm_password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: { min: 6, max: 50 },
+        errorMessage: 'Confirm password must be at least 6 characters long and less than 50 characters'
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage: 'Password must contain at least 6 characters, 1 lowercase, 1 uppercase, 1 number, and 1 symbol'
+      },
+      custom: {
+        options: (value, { req }) => value === req.body.password,
+        errorMessage: 'Passwords do not match'
+      }
+    },
+    date_of_birth: {
+      notEmpty: true,
+      toDate: true,
+      isISO8601: true,
+      errorMessage: 'Date of birth must be a valid ISODate'
+    }
+  })
+)
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,6 +5,15 @@ import { loginController, registerController } from '~/controllers/users.control
 const usersRoutes = express.Router()
 
 usersRoutes.post('/login', loginValidation, loginController)
+
+/**
+ * Description: Register a new user
+ * Route: POST /register
+ * Request body: { name: string, email: string, password: string, confirm_password:  string, date_of_birth: ISODate}
+ * Response: { message: string }
+ * Error: { error: string }
+ *
+ */
 usersRoutes.post('/register', registerValidation, registerController)
 
 export default usersRoutes
