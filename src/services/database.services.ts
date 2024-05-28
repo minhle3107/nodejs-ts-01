f58c39c2ba7dfad042acabedda280f826783ca81
minhle_3107 <lexuanminh3107@gmail.com>
2024-05-28 11:41:50 +0700

    Tôi ưu index khi khởi động server

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -32,33 +32,33 @@ class DatabaseService {
   }
 
   async indexUsers() {
-    const exists = await this.users.indexExists(['email_1_password_1', 'email_1', 'username_1'])
-    if (!exists) {
-      this.users.createIndex({ email: 1, password: 1 })
-      this.users.createIndex({ email: 1 }, { unique: true })
-      this.users.createIndex({ username: 1 }, { unique: true })
+    if (!(await this.users.indexExists(['email_1_password_1', 'email_1', 'username_1']))) {
+      await Promise.all([
+        this.users.createIndex({ email: 1, password: 1 }),
+        this.users.createIndex({ email: 1 }, { unique: true }),
+        this.users.createIndex({ username: 1 }, { unique: true })
+      ])
     }
   }
 
   async indexRefreshTokens() {
-    const exists = await this.users.indexExists(['token_1', 'exp_1'])
-    if (!exists) {
-      this.refreshTokens.createIndex({ token: 1 })
-      this.refreshTokens.createIndex({ exp: 1 }, { expireAfterSeconds: 0 })
+    if (!(await this.users.indexExists(['token_1', 'exp_1']))) {
+      await Promise.all([
+        this.refreshTokens.createIndex({ token: 1 }),
+        this.refreshTokens.createIndex({ exp: 1 }, { expireAfterSeconds: 0 })
+      ])
     }
   }
 
   async indexVideoStatus() {
-    const exists = await this.users.indexExists('name_1')
-    if (!exists) {
-      this.videoStatus.createIndex({ name: 1 })
+    if (!(await this.videoStatus.indexExists('name_1'))) {
+      await this.videoStatus.createIndex({ name: 1 })
     }
   }
 
   async indexFollowers() {
-    const exists = await this.users.indexExists('user_id_1_followed_user_id_1')
-    if (!exists) {
-      this.followers.createIndex({ user_id: 1, followed_user_id: 1 })
+    if (!(await this.followers.indexExists('user_id_1_followed_user_id_1'))) {
+      await this.followers.createIndex({ user_id: 1, followed_user_id: 1 })
     }
   }
 
