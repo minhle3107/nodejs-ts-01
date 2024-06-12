4e40568a22bf0beead57143967a5e533768ebe6e
minhle_3107 <lexuanminh3107@gmail.com>
2024-06-12 14:20:12 +0700

    Code logic createTweetValidator

--- src/middlewares/tweets.middlewares.ts
+++ src/middlewares/tweets.middlewares.ts
@@ -10,101 +10,104 @@ const tweetTypes = numberEnumToArray(EnumTweetType)
 const tweetAudiences = numberEnumToArray(EnumTweetAudience)
 const mediaTypes = numberEnumToArray(EnumMediaType)
 export const createTweetValidator = validate(
-  checkSchema({
-    type: {
-      isIn: {
-        options: [tweetTypes],
-        errorMessage: TWEETS_MESSAGES.INVALID_TYPE
-      }
-    },
-    audience: {
-      isIn: {
-        options: [tweetAudiences],
-        errorMessage: TWEETS_MESSAGES.INVALID_AUDIENCE
-      }
-    },
-    parent_id: {
-      custom: {
-        options: (value, { req }) => {
-          const type = req.body.type as EnumTweetType
-          // Nếu `type` là retweet, comment, qoutetweet thì `parent_id` phải là `tweet_id` của tweet gốc
-          if (
-            [EnumTweetType.Retweet, EnumTweetType.Comment, EnumTweetType.QuoteTweet].includes(type) &&
-            !ObjectId.isValid(value)
-          ) {
-            throw new Error(TWEETS_MESSAGES.PARENT_ID_MUST_BE_A_VALID_TWITTER_ID)
+  checkSchema(
+    {
+      type: {
+        isIn: {
+          options: [tweetTypes],
+          errorMessage: TWEETS_MESSAGES.INVALID_TYPE
+        }
+      },
+      audience: {
+        isIn: {
+          options: [tweetAudiences],
+          errorMessage: TWEETS_MESSAGES.INVALID_AUDIENCE
+        }
+      },
+      parent_id: {
+        custom: {
+          options: (value, { req }) => {
+            const type = req.body.type as EnumTweetType
+            // Nếu `type` là retweet, comment, qoutetweet thì `parent_id` phải là `tweet_id` của tweet gốc
+            if (
+              [EnumTweetType.Retweet, EnumTweetType.Comment, EnumTweetType.QuoteTweet].includes(type) &&
+              !ObjectId.isValid(value)
+            ) {
+              throw new Error(TWEETS_MESSAGES.PARENT_ID_MUST_BE_A_VALID_TWITTER_ID)
+            }
+            // Nếu `type` là tweet thì `parent_id` phải là null
+            if (type === EnumTweetType.Tweet && value !== null) {
+              throw new Error(TWEETS_MESSAGES.PARENT_ID_MUST_BE_NULL)
+            }
+            return true
           }
-          // Nếu `type` là tweet thì `parent_id` phải là null
-          if (type === EnumTweetType.Tweet && value !== null) {
-            throw new Error(TWEETS_MESSAGES.PARENT_ID_MUST_BE_NULL)
+        }
+      },
+      content: {
+        isString: true,
+        custom: {
+          options: (value, { req }) => {
+            const type = req.body.type as EnumTweetType
+            const hashtags = req.body.hashtags as string[]
+            const mentions = req.body.mentions as string[]
+            // Nếu `type` là comment, qoutetweet, tweet và không có `mentions` và `hashtags` thì `content` phải là string và không rỗng
+            if (
+              [EnumTweetType.Comment, EnumTweetType.Tweet, EnumTweetType.QuoteTweet].includes(type) &&
+              isEmpty(hashtags) &&
+              isEmpty(mentions) &&
+              value.trim() === ''
+            ) {
+              throw new Error(TWEETS_MESSAGES.CONTENT_MUST_BE_A_NON_EMPTY_STRING)
+            }
+            // Nếu `type` là retweet thì `content` phải là `''` (rỗng)
+            if (type === EnumTweetType.Retweet && value !== '') {
+              throw new Error(TWEETS_MESSAGES.CONTENT_MUST_BE_EMPTY_STRING)
+            }
+            return true
           }
-          return true
+        }
+      },
+      hashtags: {
+        isArray: true,
+        custom: {
+          options: (value, { req }) => {
+            // Yêu cầu mỗi phần từ trong array là string
+            if (value.some((item: any) => typeof item !== 'string')) {
+              throw new Error(TWEETS_MESSAGES.HASHTAG_MUST_BE_AN_ARRAY_OF_STRING)
+            }
+            return true
+          }
+        }
+      },
+      mentions: {
+        isArray: true,
+        custom: {
+          options: (value, { req }) => {
+            // Yêu cầu mỗi phần từ trong array là user_id
+            if (value.some((item: any) => !ObjectId.isValid(item))) {
+              throw new Error(TWEETS_MESSAGES.MENTION_MUST_BE_AN_ARRAY_OF_USER_ID)
+            }
+            return true
+          }
+        }
+      },
+      medias: {
+        isArray: true,
+        custom: {
+          options: (value, { req }) => {
+            // Yêu cầu mỗi phần từ trong array là Media Object
+            if (
+              value.some((item: any) => {
+                return typeof item.url !== 'string' || !mediaTypes.includes(item.type)
+              })
+            ) {
+              throw new Error(TWEETS_MESSAGES.MEDIA_MUST_BE_AN_ARRAY_OF_MEDIA_OBJECT)
+            }
+            return true
+          }
         }
       }
     },
-    content: {
-      isString: true,
-      custom: {
-        options: (value, { req }) => {
-          const type = req.body.type as EnumTweetType
-          const hashtags = req.body.hashtags as string[]
-          const mentions = req.body.mentions as string[]
-          // Nếu `type` là comment, qoutetweet, tweet và không có `mentions` và `hashtags` thì `content` phải là string và không rỗng
-          if (
-            [EnumTweetType.Comment, EnumTweetType.Tweet, EnumTweetType.QuoteTweet].includes(type) &&
-            isEmpty(hashtags) &&
-            isEmpty(mentions) &&
-            value.trim() === ''
-          ) {
-            throw new Error(TWEETS_MESSAGES.CONTENT_MUST_BE_A_NON_EMPTY_STRING)
-          }
-          // Nếu `type` là retweet thì `content` phải là `''` (rỗng)
-          if (type === EnumTweetType.Retweet && value !== '') {
-            throw new Error(TWEETS_MESSAGES.CONTENT_MUST_BE_EMPTY_STRING)
-          }
-          return true
-        }
-      }
-    },
-    hashtags: {
-      isArray: true,
-      custom: {
-        options: (value, { req }) => {
-          // Yêu cầu mỗi phần từ trong array là string
-          if (value.some((item: any) => typeof item !== 'string')) {
-            throw new Error(TWEETS_MESSAGES.HASHTAG_MUST_BE_AN_ARRAY_OF_STRING)
-          }
-          return true
-        }
-      }
-    },
-    mentions: {
-      isArray: true,
-      custom: {
-        options: (value, { req }) => {
-          // Yêu cầu mỗi phần từ trong array là user_id
-          if (value.some((item: any) => !ObjectId.isValid(item))) {
-            throw new Error(TWEETS_MESSAGES.MENTION_MUST_BE_AN_ARRAY_OF_USER_ID)
-          }
-          return true
-        }
-      }
-    },
-    medias: {
-      isArray: true,
-      custom: {
-        options: (value, { req }) => {
-          // Yêu cầu mỗi phần từ trong array là Media Object
-          if (
-            value.some((item: any) => {
-              return typeof item.url !== 'string' || !mediaTypes.includes(item.type)
-            })
-          ) {
-            throw new Error(TWEETS_MESSAGES.MEDIA_MUST_BE_AN_ARRAY_OF_MEDIA_OBJECT)
-          }
-          return true
-        }
-      }
-    }
-  })
+    ['body']
+  )
 )
