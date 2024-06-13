90b97e8808f8ab7cbdfa92188b0eb47992faa88a
minhle_3107 <lexuanminh3107@gmail.com>
2024-06-13 12:48:14 +0700

    create hashtag

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -6,6 +6,7 @@ import RefreshToken from '~/models/shcemas/RefreshToken.schema'
 import Follower from '~/models/shcemas/Follower.schema'
 import VideoStatus from '~/models/shcemas/VideoStatus.schema'
 import Tweet from '~/models/shcemas/Tweet.schema'
+import Hashtag from '~/models/shcemas/Hashtag.schema'
 
 config()
 
@@ -81,6 +82,10 @@ class DatabaseService {
 
   get tweets(): Collection<Tweet> {
     return this.db.collection(process.env.DB_TWEETS_COLLECTION as string)
+  }
+
+  get hashtags(): Collection<Hashtag> {
+    return this.db.collection(process.env.DB_HASHTAGS_COLLECTION as string)
   }
 }
 
--- src/services/tweets.services.ts
+++ src/services/tweets.services.ts
@@ -1,16 +1,52 @@
 import { TweetRequestsBody } from '~/models/requests/Tweet.requests'
 import databaseService from '~/services/database.services'
 import Tweet from '~/models/shcemas/Tweet.schema'
-import { ObjectId } from 'mongodb'
+import { ObjectId, WithId } from 'mongodb'
+import Hashtag from '~/models/shcemas/Hashtag.schema'
 
 class TweetsService {
+  async checkAndCreateHashtags(hashtags: string[]) {
+    const hashtagsDocuments = await Promise.all(
+      hashtags.map((hashtag) => {
+        // Tìm hashtag trong database, nếu không tìm thấy thì tạo mới
+        return databaseService.hashtags.findOneAndUpdate(
+          {
+            name: hashtag
+          },
+          {
+            $setOnInsert: new Hashtag({ name: hashtag })
+          },
+          {
+            upsert: true,
+            returnDocument: 'after'
+          }
+        )
+      })
+    )
+
+    return hashtagsDocuments.filter(Boolean).map((hashtag) => (hashtag as WithId<Hashtag>)._id)
+  }
+
+  // async checkAndCreateHashtags(hashtags: string[]) {
+  //   return Promise.all(
+  //     hashtags.map((hashtag) =>
+  //       databaseService.hashtags.findOneAndUpdate(
+  //         { name: hashtag },
+  //         { $setOnInsert: new Hashtag({ name: hashtag }) },
+  //         { upsert: true, returnDocument: 'after' }
+  //       )
+  //     )
+  //   ).then((hashtagsDocuments) => hashtagsDocuments.filter(Boolean).map((hashtag) => (hashtag as WithId<Hashtag>)._id))
+  // }
+
   async createTweet(user_id: string, body: TweetRequestsBody) {
+    const hashtags = await this.checkAndCreateHashtags(body.hashtags)
     const tweet = new Tweet({
       audience: body.audience,
       content: body.content,
       type: body.type,
       parent_id: body.parent_id,
-      hashtags: [],
+      hashtags: hashtags,
       mentions: body.mentions,
       medias: body.medias,
       user_id: new ObjectId(user_id)
