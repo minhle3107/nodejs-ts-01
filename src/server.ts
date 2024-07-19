1cd0e93ba3e4ad6811076ea4e57027313f0beeb8
Minhle_3107 <lexuanminh3107@gmail.com>
2024-07-19 12:14:55 +0700

    Refactor project structure for better scalability

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,45 @@
 import express from 'express'
-import databaseService from '~/services/database.services'
-import { initFolder } from '~/utils/file'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
-import { envConfig } from '~/constants/config'
-import app from '~/app'
+import fs from 'fs'
+import path from 'path'
+import yaml from 'yaml'
+import helmet from 'helmet'
+import cors from 'cors'
+import databaseService from '~/services/database.services'
+import { initFolder } from '~/utils/file'
+import { corsOptions, envConfig, limiter } from '~/constants/config'
+import { requestLoggerMiddleware } from '~/middlewares/logging.middleware'
+import swaggerUi from 'swagger-ui-express'
+import v1Routes from '~/routes/v1'
+import { defaultErrorHandler } from '~/middlewares/error.middlewares'
 
-initFolder()
-
-const startServer = async () => {
+async function startServer() {
   try {
+    initFolder()
     await databaseService.initializeDatabase()
-    console.log('Database connected successfully.')
 
-    const PORT = envConfig.port
+    const swaggerDocument = yaml.parse(fs.readFileSync(path.resolve('openapi/_build/openapi.yaml'), 'utf-8'))
+
+    const app = express()
+
+    app.use([limiter, helmet(), cors(corsOptions), express.json(), requestLoggerMiddleware])
+    app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    app.use('/api/v1', v1Routes)
+    app.use(defaultErrorHandler)
+
     const httpServer = createServer(app)
     const io = new Server(httpServer)
 
     io.on('connection', (socket) => {
       console.log(`user ${socket.id} connected`)
-      socket.on('disconnect', () => {
-        console.log(`user ${socket.id} disconnected`)
-      })
+      socket.on('disconnect', () => console.log(`user ${socket.id} disconnected`))
     })
 
-    httpServer.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+    httpServer.listen(envConfig.port, () => console.log(`App listening on port ${envConfig.port}`))
   } catch (error) {
-    console.error('Failed to connect to the database:', error)
-    process.exit(1)
+    console.error('Failed to start the server:', error)
   }
 }
 
-startServer()
+startServer().then(() => console.log('Server started successfully'))
